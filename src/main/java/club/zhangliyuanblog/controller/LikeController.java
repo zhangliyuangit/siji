2384c3733dbe3e0a33194500a994f4a370213c17
zhangliyuan <495726603@qq.com>
2021-03-30 23:29:03 +0800

    add like article

--- src/main/java/club/zhangliyuanblog/controller/LikeController.java
+++ src/main/java/club/zhangliyuanblog/controller/LikeController.java
@@ -1,20 +1,41 @@
 package club.zhangliyuanblog.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import club.zhangliyuanblog.entity.Like;
+import club.zhangliyuanblog.service.ILikeService;
+import club.zhangliyuanblog.vo.Result;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * <p>
- *  前端控制器
- * </p>
- *
  * @author liyuan.zhang
  * @since 2021-03-11
  */
+@Api("文章点赞")
 @RestController
 @RequestMapping("/like")
 public class LikeController {
 
+    private final ILikeService iLikeService;
+
+    public LikeController(ILikeService iLikeService) {
+        this.iLikeService = iLikeService;
+    }
+
+    @ApiOperation("给一篇文章点赞")
+    @PutMapping("/like/{articleId}/{currentUserId}")
+    public void likeArticle(@PathVariable Integer articleId, @PathVariable Integer currentUserId) {
+        iLikeService.save(new Like(null, currentUserId, articleId));
+    }
+
+    @ApiOperation("取消点赞")
+    @DeleteMapping("/noLike/{articleId}/{currentUserId}")
+    public void noLikeArticle(@PathVariable Integer articleId, @PathVariable Integer currentUserId) {
+        iLikeService.remove(new QueryWrapper<Like>()
+                .eq("user_id", currentUserId)
+                .eq("article_id", articleId));
+    }
 }
--- src/main/java/club/zhangliyuanblog/entity/Like.java
+++ src/main/java/club/zhangliyuanblog/entity/Like.java
@@ -6,8 +6,10 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * <p>
@@ -21,6 +23,8 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = false)
 @TableName("s_like")
 @ApiModel(value="Like对象", description="")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Like implements Serializable {
 
     private static final long serialVersionUID = 1L;
